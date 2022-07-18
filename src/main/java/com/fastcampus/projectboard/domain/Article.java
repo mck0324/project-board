650d172490ceb710033426cecc66bc01f5c4b80e
minchankim <dch3030@naver.com>
2022-07-18 14:12:59 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,7 +1,19 @@
 package com.fastcampus.projectboard.domain;
 
+<<<<<<< Updated upstream
 import java.time.LocalDateTime;
 
+=======
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Table(indexes =  )
+>>>>>>> Stashed changes
 public class Article {
     private Long id;
     private String title; // 제목
@@ -12,4 +24,8 @@ public class Article {
     private String createdBy; // 생성자
     private LocalDateTime modifiedAt; // 수정일시
     private String modifiedBy; // 수정자
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
